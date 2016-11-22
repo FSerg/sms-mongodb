b0708fbd2742cc9bea49e9d8ddf571e184c56f5b
Sergey Filkin <fs@alternativa-pro.ru>
2016-11-22 14:46:06 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "port": process.env.PORT || 5000,
+  "MONGO_URL": process.env.MONGO_URL || "mongodb://localhost:27017/testdb",
+  "token": process.env.TOKEN,
+
+  "sms_sender": process.env.SMS_SENDER || "SOME NAME",
+  "sms_username": process.env.SMS_USERNAME,
+  "sms_password": process.env.SMS_PASSWORD,
+  "sms_url": process.env.SMS_URL || "http://ws1.smsdelivery.ru/SMSWebservice.asmx/SendMessage",
+};
